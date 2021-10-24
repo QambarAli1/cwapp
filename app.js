7722cc5b456366b8cb455b326603aad76b628533
QambarAli1 <qamberali4904@gmail.com>
2021-10-24 23:56:59 +0500

    updated

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+// const port = 5000;
+const PORT = process.env.PORT || 5000
 const mongoose = require('mongoose');
 const postModel = require('./schema')
-// const db_uri = "mongodb+srv://admin:admin@cluster0.mhpe2.mongodb.net/test"
+var cors = require('cors')
+
+app.use(cors(["localhost:5000", "localhost:3000"]))
+app.use(express.json())
+
 const db_uri = "mongodb+srv://admin:admin@cluster0.gux0y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 mongoose.connect(db_uri,{
     useNewUrlParser: true,
